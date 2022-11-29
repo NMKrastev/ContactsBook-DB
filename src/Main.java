e525ef860f596cf26d0c8d926a61982b6d97ef01
Nikola <nikola.malinov.krastev@gmail.com>
2022-11-29 13:42:19 +0200

    Added contats menu in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Contacts Book Menu");
+        while (true) {
+            System.out.printf("Press 1 to add contact information.\n" +
+                    "Press 2 to show all contacts information.\n" +
+                    "Press 3 to show contact information by its phone number.\n" +
+                    "Press 4 to delete contact information.\n" +
+                    "Press 5 to update contact information\n" +
+                    "Press 6 to Exit Menu.\n");
+            System.out.println("Enter your command:");
+            int command = Integer.parseInt(scanner.nextLine());
+            switch (command) {
+                case 1:
+                    System.out.println("Add contact");
+                    break;
+                case 2:
+                    System.out.println("Show all contacts information");
+                    break;
+                case 3:
+                    System.out.println("Show contact information by its number");
+                    break;
+                case 4:
+                    System.out.println("Delete contact");
+                    break;
+                case 5:
+                    System.out.println("Update contact");
+                    break;
+                case 6:
+                    System.out.println("Exited contacts menu");
+                    System.exit(0);
+                default:
+                    System.out.println("Please, enter valid command!");
+            }
+        }
     }
 }
